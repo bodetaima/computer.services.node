fea83d47db5a884008aec56230d2e83fadd3f6a3
Phong Tran <tranphongbb@icloud.com>
2020-06-27 17:43:23 +0700

    clean up

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 let corsOptions = {
-    origin: "http://localhost:1026",
+    origin: "http://localhost:8080",
 };
 
 app.use(cors(corsOptions));

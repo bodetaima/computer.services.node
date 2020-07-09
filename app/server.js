5b67d4f246376c448eccc919a10d3a24be277f2f
Phong Tran <tranphongbb@icloud.com>
2020-07-09 19:40:46 +0700

    allow cors

--- app/server.js
+++ app/server.js
@@ -4,20 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
-const origin = "https://computer-service-cms.herokuapp.com/";
-// const origin = "http://localhost:1026"
-
-let corsOptions = {
-    origin: origin,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/", express.static("dist"));
 
 const db = require("./models");
 const Role = db.role;

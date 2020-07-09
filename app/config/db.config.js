7c5e26551aa5122632b3327a86463e70a24ecd87
Phong Tran <tranphongbb@icloud.com>
2020-07-09 18:17:52 +0700

    minor fixes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
-const url = "mongodb://ds133086.mlab.com:33086/computer_services";
+const url = "mongodb://heroku_w27m51fw@ds133086.mlab.com:33086/heroku_w27m51fw";
 // const url = "mongodb://localhost:27017/computer_services";
 
 module.exports = {

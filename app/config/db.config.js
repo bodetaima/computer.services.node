fc4fb5c4841c0823d405f0d7a9db2a9768644c72
Phong Tran <tranphongbb@icloud.com>
2020-07-09 18:12:44 +0700

    minor fixes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,6 @@
+const url = "mongodb://ds133086.mlab.com:33086/computer_services";
+// const url = "mongodb://localhost:27017/computer_services";
+
 module.exports = {
-    url: "mongodb://localhost:27017/computer_services"
-}
+    url: url,
+};

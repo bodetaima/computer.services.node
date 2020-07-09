b46688da8534d69b540b54d1e77489b1cdac9bbf
Phong Tran <tranphongbb@icloud.com>
2020-07-09 19:25:04 +0700

    add dist

--- app/server.js
+++ app/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("dist"));
+app.use("/", express.static("dist"));
 
 const db = require("./models");
 const Role = db.role;
